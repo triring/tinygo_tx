c89539ccd9e1a2f4d81bb8e6f2443f5132a4b110
triring <akio@triring.net>
2025-05-08 22:09:50 +0900

    demoのbuild.shを修正

--- demo/build.sh
+++ demo/build.sh
@@ -1,11 +1,5 @@
-tinygo build -o sendAM.uf2 -target=pico -size short .\sendFM\
+tinygo build -o .\sendFM\RadioPicoFM93_8MHz.uf2 -target=pico -size short .\sendFM\
 tinygo flash -target=pico -size short .\sendFM\
 
-tinygo build -o sendAM.uf2 -target=pico -size short .\sendAM\
+tinygo build -o .\sendAM\RadioPicoAM999KHz.uf2 -target=pico -size short .\sendAM\
 tinygo flash -target=pico -size short .\sendAM\
-
-tinygo build -o RadioPicoAM999KHz.uf2  -target=pico -size short .\RadioPicoAM999KHz\
-tinygo flash -target=pico -size short .\RadioPicoAM999KHz\
-
-tinygo build -o RadioPicoFM83_4MHz.uf2 -target=pico -size short .\RadioPicoFM83_4MHz\
-tinygo flash -target=pico -size short .\RadioPicoFM83_4MHz\
